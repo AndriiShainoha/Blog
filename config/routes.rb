656dea34118b2c56af214d1a8fc15be489204d5b
AndriiShainoha <55914660+AndriiShainoha@users.noreply.github.com>
2020-05-09 01:19:17 +0300

    Added changes in show view and routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,8 @@ Rails.application.routes.draw do
   root 'posts#index', as: 'home'
 
   get 'about' => 'pages#about', as: 'about'
-resources :posts do
-  resources :comments
-end
+
+  resources :posts do
+     resources :comments
+  end
 end
