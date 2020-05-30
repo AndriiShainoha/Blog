49a7725978f8cef58eef28398875fe731dfdb8db
Andrii Shainoha <andrii.shainoha.pz.2018@lpnu.ua>
2020-05-30 14:00:20 +0300

    Fixed some problems in the project

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,6 @@
 Rails.application.routes.draw do
-  devise_for :users
+  #devise_for :users
   root 'posts#index', as: 'home'
-  root to: "users#index"
 
   get 'about' => 'pages#about', as: 'about'
 
